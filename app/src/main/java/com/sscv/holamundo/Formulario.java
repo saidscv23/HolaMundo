9bba343418d90984e690a58441763e37384f6997
saidscv23 <132930553+saidscv23@users.noreply.github.com>
2023-11-13 20:59:53 -0500

    FORMULARIO

--- app/src/main/java/com/sscv/holamundo/Formulario.java
+++ app/src/main/java/com/sscv/holamundo/Formulario.java
@@ -3,6 +3,10 @@ package com.sscv.holamundo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Formulario extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class Formulario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+
+        EditText edNombres, edApellidos, edCedula,edDireccion;
+        Button btAceptar= null;
+        edNombres=findViewById(R.id.txtnombre);
+        edApellidos=findViewById(R.id.txtapellidos);
+        edCedula=findViewById(R.id.txtcedula);
+        edDireccion=findViewById(R.id.txtdireccion);
+        btAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre=edNombres.getText().toString();
+                if (!nombre.isEmpty()) {
+                    Toast.makeText(Formulario.this, "Informacion Ingresada Correcta", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
